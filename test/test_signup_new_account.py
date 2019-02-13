885e14d940d86df29c1793d2e893a8c72d8dca93
ssomkov <ssomkov@gmail.com>
2019-02-13 19:13:25 +0400

    add work with mail

--- test/test_signup_new_account.py
+++ test/test_signup_new_account.py
@@ -1,4 +1,18 @@
+import string
+import random
+
+
+def random_username(prefix, max_length):
+    symbols = string.ascii_letters
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(max_length))])
+
+
 def test_signup_new_account(app):
-    username = "user1"
+    username = random_username("user_", 10)
+    email = username + "@localhost"
     password = "test"
     app.james.james_ensure_user_exists(username, password)
+    app.signup.new_user(username, password, email)
+    app.session.login(username, password)
+    assert app.session.is_logged_in_as(username)
+    app.session.logout()
